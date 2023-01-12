8cc2741c615ba410b2d63e1059772eaad7b8407f
ozlemdelikanli <117909348+ozlemdelikanli@users.noreply.github.com>
2023-01-12 19:03:36 +0300

    Add files via upload

---
+++ tekboyut_soru4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main ()
+{
+	int n, i;
+	printf("Dizinin eleman sayisini giriniz: ");
+	scanf("%d", &n);
+	int a[n], b[n], c[n];
+	
+	
+	for(i=0; i<n; i++)
+	{
+		printf("A dizisi icin eleman giriniz: ");
+		scanf("%d", &a[i]);
+	}
+	
+	
+	for(i=0; i<n; i++)
+	{
+		b[i]=a[i]*a[i];
+		c[i]=sqrt(a[i]);
+	}
+	
+	
+	printf("B ve C dizisinin skaler carpimi: ");
+	for(i=0; i<n; i++)
+		printf("%d ", b[i]*c[i]);
+	
+		
+	return 0;
+}
